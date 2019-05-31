442dc1283e3f48076110bd92003ac2508068b301
Jon Couldridge <jcouldridge@outlook.com>
2019-05-31 20:20:29 +0100

    word cloud will rearrange less often if a rerender triggers but the data hasn't changed

--- src/components/Visualization.js
+++ src/components/Visualization.js
@@ -1,38 +1,41 @@
-import React from "react";
+import React, { memo } from "react";
 import ReactWordCloud from "react-wordcloud";
+import md5 from "js-md5";
 
-const Visualization = ({ values }) => {
-  // a value is the whole freetext value submitted
-  const weightedWords = values.reduce((a, { text }) => {
-    // split the text into single words
-    const words = text.split(/\s+/);
+const Visualization = memo(
+  ({ values }) => {
+    // a value is the whole freetext value submitted
+    const weightedWords = values.reduce((a, { text }) => {
+      // split the text into single words
+      const words = text.split(/\s+/);
 
-    words.forEach(w => {
-      const word = w.toLowerCase();
-      a[word] = (a[word] || 0) + 1;
-    });
+      words.forEach(w => {
+        const word = w.toLowerCase();
+        a[word] = (a[word] || 0) + 1;
+      });
 
-    return a;
-  }, {});
+      return a;
+    }, {});
 
-  const data = Object.keys(weightedWords).map(text => ({
-    text,
-    value: weightedWords[text]
-  }));
+    const data = Object.keys(weightedWords).map(text => ({
+      text,
+      value: weightedWords[text]
+    }));
 
-  console.log(data);
-
-  return (
-    <ReactWordCloud
-      options={{
-        rotations: 0,
-        fontFamily: "Arial",
-        fontSizes: [10, 60],
-        scale: "sqrt"
-      }}
-      words={data}
-    />
-  );
-};
+    return (
+      <ReactWordCloud
+        options={{
+          rotations: 0,
+          fontFamily: "Arial",
+          fontSizes: [10, 60],
+          scale: "sqrt"
+        }}
+        words={data}
+      />
+    );
+  },
+  (old, props) =>
+    old && md5(JSON.stringify(old.values)) === md5(JSON.stringify(props.values))
+);
 
 export default Visualization;
